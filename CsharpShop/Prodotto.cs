852aed9047cc80d2a1eaad1a613fb46b4384c194
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-08 15:47:25 +0200

    classe creata , costruttore inserito e metodo genera numero inserito

---
+++ CsharpShop/Prodotto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpShop
+{
+    //----------- CLASSE PRODOTTO ---------------
+    internal class Prodotto
+    {
+
+    //---------- DICHIARO LE VARIABILI --------------
+        private int codiceProdotto;
+        private string nomeProdotto;
+        private string descrizione;
+        private float prezzo;
+        private float iva;
+
+    //---------- COSTRUTTORE --------------
+    public Prodotto (string nomeProdotto , string descrizione , float prezzo , float iva)
+        {
+
+            this.nomeProdotto = nomeProdotto;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+
+        }
+
+        //-------------- METODO 1 CREAZIONE CODICE -------------
+
+        public int generaCodiceProdotto()
+        {
+
+            Random random = new Random();
+            int numero = random.Next(100);
+            return numero;
+
+
+        }
+
+        public void stampa()
+        {
+            Console.WriteLine(generaCodiceProdotto());
+        }
+
+        
+
+    }
+}
