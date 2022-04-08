2045bb5e4e16fcd563b868897525798dd5c9015e
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-08 16:44:20 +0200

    primo oggetto creato e metodi implementati

--- CsharpShop/Program.cs
+++ CsharpShop/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//------------ creo l'oggetto ------------
+using CsharpShop;
+
+Prodotto mioProdotto = new Prodotto("AirPods" , "Cuffie wireless" , 129.90 , "22%");
+
+mioProdotto.GeneraCodiceProdotto();
+mioProdotto.CalcolaPrezzoIva();
+mioProdotto.NomeEsteso();
+mioProdotto.StampaDati();
+
+//----------- creo un altro oggetto ---------
+
+
+
